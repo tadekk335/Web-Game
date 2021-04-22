f03e6a3e29f45bc6bb4845692b28a4d298a17cde
tadekk335 <57502574+tadekk335@users.noreply.github.com>
2021-04-22 18:55:06 +0100

    Add files via upload

--- main/main.js
+++ main/main.js
@@ -1,9 +1,9 @@
-function displayNext(fromElement, choice, conversation = "...") {
+export function displayNext(fromElement, choice, conversation = "...") {
     hideButtons(fromElement);
     toggleHidden(choice);
     displayConversation(conversation);
 }
-function hideButtons(fromElement) {
+export function hideButtons(fromElement) {
     const parent = document.getElementById(fromElement);
     const matches = parent.querySelectorAll("button");
     matches.forEach(element => {
@@ -11,12 +11,12 @@ function hideButtons(fromElement) {
     });
 }
 
-function toggleHidden(element) {
+export function toggleHidden(element) {
     const div = document.getElementById(element);
     div.classList.toggle("hidden");
 }
 
-function displayConversation(text) {
+export function displayConversation(text) {
     const bubble = document.getElementById("speech");
     bubble.innerHTML = text;
 }
